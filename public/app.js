3d239ff138717b1008c23a6167a2b4d216828faa
gabkoi21 <12345678+username@users.noreply.github.com>
2024-12-02 01:56:07 +0000

    The add the contact form

--- public/app.js
+++ public/app.js
@@ -3,12 +3,24 @@ const closeButton = document.getElementById("close-button");
 const navbarHamburger = document.getElementById("navbar-hamburger");
 
 // Toggle navbar on hamburger button click
-hamburgerButton.addEventListener("click", () => {
+hamburgerButton.addEventListener("click", (event) => {
+  event.stopPropagation();
   navbarHamburger.classList.add("active");
   navbarHamburger.slider.classList.add("slide");
 });
 
 // Close navbar on close button click
-closeButton.addEventListener("click", () => {
+closeButton.addEventListener("click", (event) => {
+  event.stopPropagation();
   navbarHamburger.classList.remove("active");
+});
+
+// Close navbar when clicking anywhere outside
+document.addEventListener("click", (event) => {
+  if (
+    !navbarHamburger.contains(event.target) &&
+    !hamburgerButton.contains(event.target)
+  ) {
+    navbarHamburger.classList.remove("active");
+  }
 });
