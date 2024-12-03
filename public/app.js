2a4988d10b7ef82cc32e5705575a611e89db22b7
gabkoi21 <12345678+username@users.noreply.github.com>
2024-12-03 03:40:37 +0000

    add the footer and edit the nav

--- public/app.js
+++ public/app.js
@@ -24,3 +24,33 @@ document.addEventListener("click", (event) => {
     navbarHamburger.classList.remove("active");
   }
 });
+
+// Show button when scrolled down
+const backToTopButton = document.getElementById("back-to-top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+// Scroll to top on click
+backToTopButton.addEventListener("click", () => {
+  document.getElementById("header").scrollIntoView({ behavior: "smooth" });
+});
+
+window.onscroll = function () {
+  changeNavbarColor();
+};
+
+// this is for the blsck bg
+// JavaScript to detect scroll and add/remove shadow
+window.onscroll = function () {
+  var navbar = document.getElementById("navbar");
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+};
